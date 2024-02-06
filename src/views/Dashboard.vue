71d51056a52399115640f1b9e278da2bb29d2986
raheego <raae517@gmail.com>
2024-02-06 21:10:20 +0900

    dashboard 태그정렬

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,30 +5,8 @@
       <Attendance />
     </div>
 
-    <div class="row">
+    <div class="row py-4">
       <div class="col-lg-12">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 col-12">
-            <card :title="stats.money.title" :value="stats.money.value" :percentage="stats.money.percentage"
-              :iconClass="stats.money.iconClass" :iconBackground="stats.money.iconBackground" :detail="stats.money.detail"
-              directionReverse></card>
-          </div>
-          <div class="col-lg-3 col-md-6 col-12">
-            <card :title="stats.users.title" :value="stats.users.value" :percentage="stats.users.percentage"
-              :iconClass="stats.users.iconClass" :iconBackground="stats.users.iconBackground" :detail="stats.users.detail"
-              directionReverse></card>
-          </div>
-          <div class="col-lg-3 col-md-6 col-12">
-            <card :title="stats.clients.title" :value="stats.clients.value" :percentage="stats.clients.percentage"
-              :iconClass="stats.clients.iconClass" :iconBackground="stats.clients.iconBackground"
-              :percentageColor="stats.clients.percentageColor" :detail="stats.clients.detail" directionReverse></card>
-          </div>
-          <div class="col-lg-3 col-md-6 col-12">
-            <card :title="stats.sales.title" :value="stats.sales.value" :percentage="stats.sales.percentage"
-              :iconClass="stats.sales.iconClass" :iconBackground="stats.sales.iconBackground" :detail="stats.sales.detail"
-              directionReverse></card>
-          </div>
-        </div>
         <div class="row">
           <div class="col-lg-7 mb-lg">
             <!-- line chart -->
@@ -41,68 +19,29 @@
           </div>
         </div>
         <div class="row mt-4">
-          <!-- <div class="col-lg-7 mb-lg-0 mb-4">
-            <div class="card">
-              <div class="p-3 pb-0 card-header">
-                <div class="d-flex justify-content-between">
-                  <h6 class="mb-2">Sales by Country</h6>
-                </div>
-              </div>
-              <div class="table-responsive">
-                <table class="table align-items-center">
-                  <tbody>
-                    <tr v-for="(sale, index) in sales" :key="index">
-                      <td class="w-30">
-                        <div class="px-2 py-1 d-flex align-items-center">
-                          <div>
-                            <img :src="sale.flag" alt="Country flag" />
-                          </div>
-                          <div class="ms-4">
-                            <p class="mb-0 text-xs font-weight-bold">Country:</p>
-                            <h6 class="mb-0 text-sm">{{ sale.country }}</h6>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="text-center">
-                          <p class="mb-0 text-xs font-weight-bold">Sales:</p>
-                          <h6 class="mb-0 text-sm">{{ sale.sales }}</h6>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="text-center">
-                          <p class="mb-0 text-xs font-weight-bold">Value:</p>
-                          <h6 class="mb-0 text-sm">{{ sale.value }}</h6>
-                        </div>
-                      </td>
-                      <td class="text-sm align-middle">
-                        <div class="text-center col">
-                          <p class="mb-0 text-xs font-weight-bold">Bounce:</p>
-                          <h6 class="mb-0 text-sm">{{ sale.bounce }}</h6>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>  -->
-          <Joo />
+          <div class="col-lg-7 mb-lg">
+            <Joo />
+          </div>
           <div class="col-lg-5">
-            
             <Yjinlee />
           </div>
         </div>
       </div>
     </div>
+
+    <div class="row py-4" >
+      <div class="col-12">
+        <authors-table />
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import Card from "@/examples/Cards/Card.vue";
 import Carousel from "./components/Carousel.vue";
 import Attendance from "./components/Attendance.vue";
 import Joo from "./components/Joo.vue";
 import Commute from "./components/Commute.vue";
+import AuthorsTable from "./components/AuthorsTable.vue";
 
 import US from "@/assets/img/icons/flags/US.png";
 import DE from "@/assets/img/icons/flags/DE.png";
@@ -115,41 +54,7 @@ export default {
   name: "dashboard-default",
   data() {
     return {
-      stats: {
-        money: {
-          title: "Today's Money",
-          value: "$53,000",
-          percentage: "+55%",
-          iconClass: "ni ni-money-coins",
-          detail: "since yesterday",
-          iconBackground: "bg-gradient-primary",
-        },
-        users: {
-          title: "Today's Users",
-          value: "2,300",
-          percentage: "+3%",
-          iconClass: "ni ni-world",
-          iconBackground: "bg-gradient-danger",
-          detail: "since last week",
-        },
-        clients: {
-          title: "New Clients",
-          value: "+3,462",
-          percentage: "-2%",
-          iconClass: "ni ni-paper-diploma",
-          percentageColor: "text-danger",
-          iconBackground: "bg-gradient-success",
-          detail: "since last quarter",
-        },
-        sales: {
-          title: "Sales",
-          value: "$103,430",
-          percentage: "+5%",
-          iconClass: "ni ni-cart",
-          iconBackground: "bg-gradient-warning",
-          detail: "than last month",
-        },
-      },
+
       sales: {
         us: {
           country: "United States",
@@ -183,12 +88,12 @@ export default {
     };
   },
   components: {
-    Card,
     Carousel,
     Yjinlee,
     Attendance,
     Joo,
     Commute,
+    AuthorsTable
   },
 };
 </script>
